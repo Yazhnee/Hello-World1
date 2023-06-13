269b8ec073ffdf9511a4550a14d45db8ff058529
Yazhnee <yazhnee@gmail.com>
2023-06-13 22:12:45 +0530

    Solving Selenium compilation issues

--- FirstScript.java
+++ FirstScript.java
@@ -1,11 +1,5 @@
-package day1;
+//package day1;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstScript  {
 
@@ -13,14 +7,14 @@ public class FirstScript  {
 		// TODO Auto-generated method stub
 
 		//set the path of executable browser driver
-		System.setProperty("webdriver.chrome.driver",
+		/*System.setProperty("webdriver.chrome.driver",
 				"C:\\Yazhnee\\Technical_Workbook\\Selenium\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();//WebDriver- Parent & ChromeDriver-Child
 		driver.manage().deleteAllCookies();
 		driver.manage().window().fullscreen();
-		driver.get("https://www.amazon.in/");
-		System.out.println(driver.getTitle());//Printing title
-		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("puma shoes for women");//id taken from Amazon xml page
+		driver.get("https://www.amazon.in/");*/
+		System.out.println("Hello World");//Printing title
+		/*driver.findElement(By.id("twotabsearchtextbox")).sendKeys("puma shoes for women");//id taken from Amazon xml page
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		/*
 		 * synchronized (driver) {
@@ -29,7 +23,9 @@ public class FirstScript  {
 		 * e) { // TODO Auto-generated catch block e.printStackTrace(); } //10
 		 * seconds=10000 milliseconds } //driver.manage().timeouts().implicitlyWait(30,
 		 * TimeUnit.SECONDS);
-		 */		driver.quit();
+		 */		
+		// driver.quit();
+		
 	}
 
 	/*
